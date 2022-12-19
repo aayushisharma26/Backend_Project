941a3dfc4a428ebe016cd0bf97d1f8b732e86985
jabina01 <jabeenabano20@navgurukul.org>
2022-12-19 17:13:54 +0530

    first commit

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express=require("express")
 
 const mongoose=require("mongoose")
 
+const cors=require("cors")
+
 require('dotenv').config()
 
 const router=require('./Router/Router.js')
 
 const app=express()
+
+app.use(cors())
 
 app.use(express.json())
 
